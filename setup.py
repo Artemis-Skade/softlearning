0ad2307a7f4f5941c15dc7caf78dac77a7576d0e
noah <n.dormann@tum.de>
2020-11-20 09:59:30 +0100

    try to remove mujoco dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'dm-control>=0.0.322773188',
         'gtimer>=1.0.0b5',
         'gym>=0.17.2',
-        'mujoco-py>=2.0.2.10',
         'numpy>=1.17.5',
         'pandas',
         'ray[tune]>=1.0.0',
